0d3afc1a09f85ba330382ff3ee46be7808540a3f
Bluebarrycat <barry.kr.campbell@gmail.com>
2024-08-21 20:38:06 -0500

    Testing new button monitoring

---
+++ button_monitortwo.py
@@ -0,0 +1,43 @@
+# from gpiozero import Button # imports button from the Pi GPIO library
+# import time
+# import os # imports OS library for Shutdown control
+# from main import * # This imports the main program
+
+# stopButton = Button(27) # defines the button as an object and chooses GPIO 27
+# restartProgram = Button(22)
+
+# while True: # infinite loop
+# 	if stopButton.is_pressed or restartProgram.is_pressed: # Check to see if button is pressed
+# 		time.sleep(1) # wait for the hold time we want.
+# 		if stopButton.is_pressed: # check if the user let go of the button
+# 			os.system("sudo shutdown now -h") # shut down the Pi -h is or -r will reset
+# 		elif restartProgram.is_pressed:
+# #			pkill -9 -f automate_fermentation.py
+# #			time.sleep(1)
+# #			temp_display()
+# 			print("this button is pressed")
+# #		else:
+# #			main()
+
+
+# 	time.sleep(1) # wait to loop again so we don't use the processor too much.
+
+from gpiozero import Button
+import subprocess
+from signal import pause
+import sys
+
+def restart_program():
+   try:
+      subprocess.run(['python', 'main.py'])
+   except KeyboardInterrupt:
+      print("Exiting script...")
+      sys.exit()
+   finally:
+      sys.exit()
+
+restart_button = Button(27)
+
+restart_button.when_pressed = restart_program
+
+pause()
